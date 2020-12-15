58585a34a60a402728a83af82f67c8110c402f05
inayahwidya03 <76075457+inayahwidya03@users.noreply.github.com>
2020-12-16 05:36:21 +0700

    Add files via upload

---
+++ data/Singosariacuan_1.js
@@ -0,0 +1,8 @@
+var json_Singosariacuan_1 = {
+"type": "FeatureCollection",
+"name": "Singosariacuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 112.664584661389725, -7.895082198461436 ] } }
+]
+}
